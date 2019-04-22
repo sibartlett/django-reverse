0f9b511d0834437e00a0ba2391a6d0470a55d8b6
Alex Lepsa <lepsalex@gmail.com>
2019-04-22 16:31:20 -0400

    Revised types

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
 declare module 'django-reverse' {
-  function reverse(...args: (string|number)[]): string|void;
+  function reverse(...args: (string | number)[]): string | string[];
   export = reverse;
 }
